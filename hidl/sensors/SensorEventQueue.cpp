10f950e662055fa630d3980ecb56ce06c5fba680
Binay Tiwari <tbinay40@gmail.com>
2020-03-07 09:15:34 +0545

    a5ultexx: Import Sensor HAL HIDL
    * Imported from https://github.com/LineageOS/android_device_samsung_msm8974-common

--- hidl/sensors/SensorEventQueue.cpp
+++ hidl/sensors/SensorEventQueue.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,13 @@
  * limitations under the License.
  */
 
-#include <hardware/sensors.h>
-#include <algorithm>
 #include <pthread.h>
+
+#include <algorithm>
+
 #include <log/log.h>
 
+#include <hardware/sensors.h>
 #include "SensorEventQueue.h"
 
 SensorEventQueue::SensorEventQueue(int capacity) {
